d580790e7585e4d559a5cfd50007d75298fdb8d2
nickdanger3d <nick@bonfatti.net>
2015-06-27 19:16:34 -0400

    database

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config.StagingConfig')
+db = SQLAlchemy(app)
+
+from models import Hitter, Team, Game
 
 @app.route('/')
 def hello():
