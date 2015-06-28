d72a897778f96e52368052adb86029163fc0e302
nickdanger3d <nick@bonfatti.net>
2015-06-27 20:08:02 -0400

    restless

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+import flask.ext.sqlalchemy
+import flask.ext.restless
 
 app = Flask(__name__)
 app.config.from_object('config.StagingConfig')
-db = SQLAlchemy(app)
+db = flask.exst.sqlalchemy.SQLAlchemy(app)
 
 from models import Hitter, Team, Game
+
+restless_manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+restless_manager.create_api(Hitter, methods=['GET', 'POST', 'DELETE'])
+restless_manager.create_api(Game, methods=['GET', 'POST', 'DELETE'])
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -23,3 +23,5 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
+
+#print(os.environ['DATABASE_URL'])
