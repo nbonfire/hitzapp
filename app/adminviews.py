7d9844ff0a380028067617b401d995dc475690d3
nickdanger3d <nick@bonfatti.net>
2015-07-25 17:45:23 -0400

    adding/fixing restore function

--- app/adminviews.py
+++ app/adminviews.py
@@ -8,42 +8,42 @@ from models import Hitter, Game, get_or_create, completeGame
 
 
 class HitzAdminView(BaseView):
-	'''def is_accessible(self):
+    '''def is_accessible(self):
         return login.current_user.is_authenticated()
     def _handle_view(self, name, **kwargs):
         if not self.is_accessible():
             return redirect(url_for('login', next=request.url))
-	'''
-	@expose('/')
-	def index(self):
-		return self.render('index.html')
+    '''
+    @expose('/')
+    def index(self):
+        return self.render('index.html')
 
 class HitterRuleView(ModelView):
-	column_formatters=dict(overallrating=lambda v, c, m, p: m.overallrating.mu - (m.overallrating.sigma * 3), rating=lambda v, c, m, p: m.rating.mu - (m.rating.sigma * 3))
+    column_formatters=dict(overallrating=lambda v, c, m, p: m.overallrating.mu - (m.overallrating.sigma * 3), rating=lambda v, c, m, p: m.rating.mu - (m.rating.sigma * 3))
 
 class GameRuleView(ModelView):
-	from_create_rules = ('awayteam','awaypoints',rules.Text(' vs. '),'hometeam', 'homepoints', 'winner', 'date', 'event' )
+    from_create_rules = ('awayteam','awaypoints',rules.Text(' vs. '),'hometeam', 'homepoints', 'winner', 'date', 'event' )
 
 class GameModelView(ModelView):
-	column_display_all_relations=True
-	column_list = ('awayteam', 'awaypoints','hometeam', 'homepoints', 'winner', 'date', 'event' )
+    column_display_all_relations=True
+    column_list = ('awayteam', 'awaypoints','hometeam', 'homepoints', 'winner', 'date', 'event' )
 
 class HitterFileAdmin(FileAdmin):
-	allowed_extensions=('txt')
-	def on_file_upload(self, directory, path, filename):
-		names=[]
-		with io.open(filename, 'r', encoding='utf-8') as fn:
-        	names=json.loads(fn.read())
+    allowed_extensions=('txt')
+    def on_file_upload(self, directory, path, filename):
+        names=[]
+        with io.open(filename, 'r', encoding='utf-8') as fn:
+            names=json.loads(fn.read())
 
         for name in names:
-        	get_or_create(db.session, Hitter, name=name)
+            get_or_create(db.session, Hitter, name=name)
 
 class GameFileAdmin(FileAdmin):
-	allowed_extensions=('txt')
-	def on_file_upload(self, directory, path, filename):
-		results=[]
-		with io.open(filename, 'r', encoding='utf-8') as fg:
-        	results=json.loads(fg.read())
+    allowed_extensions=('txt')
+    def on_file_upload(self, directory, path, filename):
+        results=[]
+        with io.open(filename, 'r', encoding='utf-8') as fg:
+            results=json.loads(fg.read())
 
         for game in results:
-        	completeGame(db.session, game['home'], game['away'], game['winner'], game['score']['away'], game['score']['home'], datetime.strptime(game['date'], '%Y-%m-%d'))
+            completeGame(db.session, game['home'], game['away'], game['winner'], game['score']['away'], game['score']['home'], datetime.strptime(game['date'], '%Y-%m-%d'))
